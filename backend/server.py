57e0de00a2014f1891d5c224fda3e099840792b0
OHA2025g <dt692664@gmail.com>
2025-08-26 18:31:55 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -24,7 +24,7 @@ api_router = APIRouter(prefix="/api")
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=["*"],
+    allow_origins=["https://menindata.org"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
